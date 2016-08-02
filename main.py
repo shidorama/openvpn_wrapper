d1be1adf5adf26b9296131a4ba245517e81c045a
Stanislav Zakrevskiy <sz@bekitzur.com>
2016-08-02 15:25:36 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+import sys
+import os
+import time
+import pty
+from pyotp import totp
+
+config = {
+    "username": '',
+    "password": '',
+    "google_secret": ""
+}
+
+def openvpn_wrapper():
+  try:
+    ( child_pid, fd ) = pty.fork()
+  except OSError as e:
+    print str(e)
+
+  if child_pid == 0:
+    print "In Child Process: PID# %s" % os.getpid()
+    sys.stdout.flush()
+    try:
+      os.execlp("openvpn","OpenVPN",'--config', 'client.ovpn')
+    except:
+      print "Cannot spawn execlp..."
+  else:
+    print "In Parent Process: PID# %s" % os.getpid()
+    print os.read(fd, 1000)
+    otp_gen = totp.TOTP(s=config['google_secret'])
+    os.write(fd,config['username']+'\n')
+    print os.read(fd, 1000)
+    time.sleep(2)
+    os.write(fd,config['password']+'\n')
+    print os.read(fd, 1000)
+    time.sleep(2)
+    otp_code = otp_gen.now()
+    os.write(fd,str(otp_code)+'\n')
+    print os.read(fd, 1000)
+    time.sleep(2)
+    while True:
+      print os.read(fd, 10000)
+
+
+if __name__ == "__main__":
+    openvpn_wrapper()
