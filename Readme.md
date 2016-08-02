459ce659bdc6b5477ed0d8d0d568a5991ab69bf6
Stanislav Zakrevskiy <sz@bekitzur.com>
2016-08-02 15:42:38 +0300

    Added readme, added some sanity checks

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# Installation
+1. clone
+1. pip install -r requirements.txt
+
+# Configuaration
+1. in main.py set `username`, `password` and `google_secret` fields
+
+# Start
+1. sudo ./main.py
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python2
 import sys
 import os
 import time
@@ -17,26 +18,34 @@ def openvpn_wrapper():
     print str(e)
 
   if child_pid == 0:
-    print "In Child Process: PID# %s" % os.getpid()
+    # print "In Child Process: PID# %s" % os.getpid()
     sys.stdout.flush()
     try:
       os.execlp("openvpn","OpenVPN",'--config', 'client.ovpn')
     except:
       print "Cannot spawn execlp..."
   else:
-    print "In Parent Process: PID# %s" % os.getpid()
+    if not isinstance(config, dict):
+      raise TypeError('config must be dictionary!')
+    for entry in config:
+      if not config[entry]:
+        raise RuntimeError('Missing config data: %s' % entry)
+    # print "In Parent Process: PID# %s" % os.getpid()
     print os.read(fd, 1000)
     otp_gen = totp.TOTP(s=config['google_secret'])
-    os.write(fd,config['username']+'\n')
-    print os.read(fd, 1000)
-    time.sleep(2)
-    os.write(fd,config['password']+'\n')
-    print os.read(fd, 1000)
-    time.sleep(2)
-    otp_code = otp_gen.now()
-    os.write(fd,str(otp_code)+'\n')
-    print os.read(fd, 1000)
-    time.sleep(2)
+    try:
+      os.write(fd,config['username']+'\n')
+      print os.read(fd, 1000)
+      time.sleep(2)
+      os.write(fd,config['password']+'\n')
+      print os.read(fd, 1000)
+      time.sleep(2)
+      otp_code = otp_gen.now()
+      os.write(fd,str(otp_code)+'\n')
+      print os.read(fd, 1000)
+      time.sleep(2)
+    except KeyError as e:
+      raise KeyError("Missing config parameter! %s"%e.message)
     while True:
       print os.read(fd, 10000)
 
